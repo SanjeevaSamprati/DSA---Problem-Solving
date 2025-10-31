5db511eca6d2ae522828f0b26c1c525876e06618
Sanjeeva <sanzsamprati232@gmail.com>
2025-10-31 23:54:08 +0530

    Leetocde 136

---
+++ Arrays/MaxOnes.java
@@ -0,0 +1,16 @@
+public class MaxOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int maxCount=0;
+
+        for(int n: nums){
+            if(n == 1){
+                count++;
+                maxCount = Math.max(maxCount , count);
+            } else {
+                count = 0;
+            }
+        }
+        return maxCount;
+    }
+}
