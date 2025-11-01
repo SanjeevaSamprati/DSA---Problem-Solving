ed4fe863a7e0ff37aac2c78ec3ab868ca21c7315
Sanjeeva <sanzsamprati232@gmail.com>
2025-11-01 17:57:04 +0530

    Strings

--- Arrays/MaxOnes.java
+++ Arrays/MaxOnes.java
@@ -1,12 +1,12 @@
-public class MaxOnes {
+class solution {
     public int findMaxConsecutiveOnes(int[] nums) {
         int count = 0;
-        int maxCount=0;
+        int maxCount = 0;
 
-        for(int n: nums){
-            if(n == 1){
+        for (int n : nums) {
+            if (n == 1) {
                 count++;
-                maxCount = Math.max(maxCount , count);
+                maxCount = Math.max(maxCount, count);
             } else {
                 count = 0;
             }
