b4285532c5bac8380afc1c499c6b3eab568d8c70
Sanjeeva <sanzsamprati232@gmail.com>
2025-11-05 19:47:40 +0530

    Lc 169

--- Strings/ValidAnagram.java
+++ Strings/ValidAnagram.java
@@ -1,24 +1,25 @@
 package Strings;
 
-public class Valid Anagram {
+public class ValidAnagram {
     public boolean isAnagram(String s, String t) {
 
-        if(s.length() != t.length()) return false;
+        if (s.length() != t.length())
+            return false;
 
         int[] freq = new int[26];
 
-        for(char c : s.toCharArray()){
+        for (char c : s.toCharArray()) {
             freq[c - 'a']++;
         }
 
-        for(char c : t.toCharArray()){
+        for (char c : t.toCharArray()) {
             freq[c - 'a']--;
         }
 
-        for(int count : freq){
-            if(count != 0) return false;
-        } 
+        for (int count : freq) {
+            if (count != 0)
+                return false;
+        }
         return true;
     }
 }
-
